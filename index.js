97e70192cdd79e899c20c0405a271ca5cfacdcce
Marin Nikolli <sibalonat@gmail.com>
2022-11-16 12:49:44 +0100

    legacy purposes

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const mongoose = require('mongoose')
+
+require('./models/User')
+
+const passport = require('passport')
+// require('./services/passport')
+
+// mongoose.connect('mongodb://user:pass@localhost:port/database')
+mongoose.connect('mongodb+srv://root:marini12345@usersauthenticatewithgo.3kdehte.mongodb.net/?retryWrites=true&w=majority', 
+{       
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
+
+const app = express()
+app.use(passport.initialize())
+
+require('./routes/authRoutes')(app)
+
+
+const port = 5000
+app.listen(port)
---
+++ services/passport.js
@@ -0,0 +1,22 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+const moongose = require('mongoose')
+
+const User = require('user')
+const passport = require('passport')
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user)
+    })
+})
+
+
+
+
+
+
